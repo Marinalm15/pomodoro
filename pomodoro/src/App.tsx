188b9b72effd0580e1e452118a7df63c07823cb4
Marinalm15 <marinalm15@hotmail.com>
2023-12-04 19:27:58 -0300

    install react-router-dom and routing implementation

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -1,10 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './components/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './components/Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle></GlobalStyle>
     </ThemeProvider>
   )
